73226eb42186247ad119d1489c899eaa8b8935a3
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2021-10-06 17:39:31 -0600

    Grafica leisouve

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ Informacion importante
 Aceleracion Entre placas: a = abs(q)*V/(md)
 '''
 import math
+import time
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation as animation
+import threading
 
 from matplotlib import pyplot as plt
 
@@ -47,6 +52,10 @@ tiempo_etapa_2 = distancia_entre_placasVH / velocidad_rayo_z
 tiempo_etapa_3 = tiempo_etapa_1
 tiempo_etapa_4 = distancia_entre_placaH_pantalla / velocidad_rayo_z
 potenical_aceptado = 1.2015 * pow(10, 14)
+tonalidades_de_rayo = ["#F4ECF7",  '#E8DAEF', '#D2B4DE',
+                       '#BB8FCE', '#A569BD', '#8E44AD',
+                       '#7D3C98', '#6C3483', '#5B2C6F',
+                       '#4A235A']
 
 
 def calculo_de_movimiento_x(potencial_placa_vertical):
@@ -69,6 +78,12 @@ def comprobacion_de_voltaje(potencial_placa_horizontal):
 
 
 def trayectoria_de_una_particula(potencial_placa_vertical, potencial_placa_horizontal):
+    '''
+
+    :param potencial_placa_vertical:
+    :param potencial_placa_horizontal:
+    :return:
+    '''
     if comprobacion_de_voltaje(potencial_placa_horizontal) and comprobacion_de_voltaje(potencial_placa_vertical):
         coordenada_x = calculo_de_movimiento_x(potencial_placa_vertical)
         coordenada_y = calculo_de_movimiento_y(potencial_placa_horizontal)
@@ -78,20 +93,86 @@ def trayectoria_de_una_particula(potencial_placa_vertical, potencial_placa_horiz
         return [0, 0]
 
 
-def figuras_de_lisajous(velocidad_angular_x, velocidad_angular_y, desplazamiento_y):
-    periodo_1 = math.pi * 2 / velocidad_angular_x
-    periodo_2 = math.pi * 2 / velocidad_angular_x
-    if periodo_1 < periodo_2:
-        periodo_1 = periodo_2
-    tiempo = 0
-    intervalo_tiempo = periodo_1 / 1000
+def figuras_de_lisajous(velocidad_angular_x, velocidad_angular_y, desplazamiento_y, continuidad):
+    '''
+
+    :param velocidad_angular_x: velocidad angular para MAS en x  Del 1 al 5
+    :param velocidad_angular_y: velocidad angular para MAS en y  Del 1 al 6
+    :param desplazamiento_y: desplazamiento para movimiento y segun ecuacion en MAS, valores recomendados: 0, pi/4, pi/2, 3pi/4, pi
+    :param continuidad: ingresar 100
+    :return: void, pero genera grafica
+    '''
+    '''
+    grafica
+    '''
+    gData = []
     coordenadas_x = []
+    gData.append(coordenadas_x)
     coordenadas_y = []
-    coordenadas_x.append(calculo_de_movimiento_x(potenical_aceptado * (math.sin(velocidad_angular_x * tiempo))))
-    coordenadas_y.append(
-        calculo_de_movimiento_y(potenical_aceptado * (math.sin(velocidad_angular_y * tiempo + desplazamiento_y))))
-    while not tiempo == periodo_1:
-        tiempo += intervalo_tiempo
-        coordenadas_x.append(calculo_de_movimiento_x(potenical_aceptado * (math.sin(velocidad_angular_x * tiempo))))
-        coordenadas_y.append(
-            calculo_de_movimiento_y(potenical_aceptado * (math.sin(velocidad_angular_y * tiempo + desplazamiento_y))))
+    gData.append(coordenadas_y)
+    '''
+    Toda la generacion de datos
+    '''
+    def GetData(out_data):
+        '''
+
+        :param out_data: lista con dos posiciones: index 0: lista eje x a graficar; index 1: eje y a graficar
+        :return:void
+        '''
+        '''
+         ELECCION DE COLOR
+         '''
+        latencia = 0
+        limite_inferior = 19
+        limite_superior = limite_inferior + 21
+        if (continuidad < 20):
+            print("La continuidad debe ser mayor a 20")
+            return
+        for r in range(len(tonalidades_de_rayo)):
+            if (limite_inferior < continuidad <= limite_superior):
+                latencia = r
+                break
+            limite_inferior += 20
+            limite_superior += 20
+            if (r == 9):
+                latencia = 9
+
+        color = tonalidades_de_rayo[latencia]
+        '''
+        ELECCIÓN DE PERIODO
+        '''
+        periodo_1 = math.pi * 2 / velocidad_angular_x
+        periodo_2 = math.pi * 2 / velocidad_angular_y
+        if (periodo_1 < periodo_2):
+            periodo_1 = periodo_2
+        '''
+        GENERACION DE PUNTOS
+        '''
+        tiempo = 0
+        intervalo_tiempo = periodo_1 / continuidad
+        out_data[0].append(calculo_de_movimiento_x(potenical_aceptado*math.cos(velocidad_angular_x * tiempo)))
+        out_data[1].append(calculo_de_movimiento_y(potenical_aceptado*math.cos(velocidad_angular_y * tiempo + desplazamiento_y)))
+        for i in range(continuidad):
+            tiempo += intervalo_tiempo
+            out_data[0].append(calculo_de_movimiento_x(potenical_aceptado * math.cos(velocidad_angular_x * tiempo)))
+            out_data[1].append(calculo_de_movimiento_y(potenical_aceptado * math.cos(velocidad_angular_y * tiempo + desplazamiento_y)))
+            time.sleep(0.1)
+
+    dataCollector = threading.Thread(target= GetData, args=(gData,) )
+    dataCollector.start()
+
+    def update_line(num, hl, data):
+        dx = np.array(data[0])
+        dy = np.array(data[1])
+        hl.set_data(dx, dy)
+        return hl,
+
+    fig = plt.figure(figsize=(10, 8))
+    plt.xlim(-0.21, 0.21)
+    plt.ylim(-0.13, 0.13)
+    hl, = plt.plot(gData[0], gData[1])
+    line_ani = animation.FuncAnimation(fig, update_line, fargs=(hl, gData ),
+                                       interval=50, blit=False)
+    plt.show()
+    dataCollector.join()
+figuras_de_lisajous(1, 2, math.pi / 2, 100)
